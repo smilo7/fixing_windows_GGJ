372fb19e82b9a9fbf8d0ad5b859da435da0b49b9
williamsonmaxwell <60532229+williamsonmaxwell@users.noreply.github.com>
2020-02-01 13:25:26 +0000

    Implementations
    -Sprint
    -Jump?
    -Ceiling
    -redone clipping of objects
    -added hud but still needs programming

--- fixingWindows/Player.gd
+++ fixingWindows/Player.gd
@@ -2,8 +2,8 @@ extends KinematicBody
 
 
 #const MOVE_SPEED = 8
-#const SPRINT_SPEED = 2
-#const MAX_JUMP_TIME = 10
+const SPRINT_SPEED = 1.05
+const MAX_JUMP_TIME = 2
 const MOUSE_SENS = 0.5
 const MOVE_SPEED = 6
 const ACCELERATION = 3
@@ -14,7 +14,10 @@ onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
 var gravity = -9.8
 var velocity = Vector3()
+var newVel = Vector3()
 var rotate = 0
+var jump_time = 0
+var falling = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,14 +39,11 @@ func _process(delta):
 		get_tree().quit()
 	if Input.is_action_pressed("restart"):
 		kill()
-
-
 	 
-
 func _physics_process(delta):
 	var move_vec = Vector3()
 	var speed_Modifier = 1
-
+	
 	if Input.is_action_pressed("move_forwards"):
 		move_vec.z -= 1
 	if Input.is_action_pressed("move_backwards"):
@@ -73,20 +73,24 @@ func _physics_process(delta):
 	velocity.x = hv.x
 	velocity.z = hv.z
 	
+	if Input.is_action_pressed("sprint"):
+		speed_Modifier = SPRINT_SPEED
 	
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
-#	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-#	move_and_collide(move_vec * (MOVE_SPEED * speed_Modifier) * delta)
-   
+#	if Input.is_action_just_pressed("jump") and falling != true:
+#		velocity.y = 5 * speed_Modifier
+
+	
+	velocity = move_and_slide(velocity * speed_Modifier, Vector3(0,1,0))
+	
 	if Input.is_action_pressed("attack") and !anim_player.is_playing():
 		anim_player.play("attack")
 		var coll = raycast.get_collider()
 		if raycast.is_colliding() and coll.has_method("kill"): #if object it is colliding with has a kill method then kill it
 			coll.kill()
- 
+	
 func kill():
 	get_tree().reload_current_scene()
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
