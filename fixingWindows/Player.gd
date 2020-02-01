53cc84f9931aa17caf78ac1e85b71d34143f4825
williamsonmaxwell <60532229+williamsonmaxwell@users.noreply.github.com>
2020-02-01 11:11:10 +0000

    movement
    Made the mouse move and and made the player move and and sprint. Sprite work .

--- fixingWindows/Player.gd
+++ fixingWindows/Player.gd
@@ -1,14 +1,20 @@
 extends KinematicBody
 
 
-const MOVE_SPEED = 8
+#const MOVE_SPEED = 8
+#const SPRINT_SPEED = 2
+#const MAX_JUMP_TIME = 10
 const MOUSE_SENS = 0.5
+const MOVE_SPEED = 6
+const ACCELERATION = 3
+const DE_ACCELERATION = 5
+
 
 onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
-
- 
-
+var gravity = -9.8
+var velocity = Vector3()
+var rotate = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,16 +27,23 @@ func _ready():
 #for mouse movement
 func _input(event):
 	if event is InputEventMouseMotion:
+		rotation_degrees.x -= MOUSE_SENS * event.relative.y
 		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+		rotate -= (MOUSE_SENS * event.relative.x) * 3.141593 / 180
 
 func _process(delta):
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 	if Input.is_action_pressed("restart"):
 		kill()
- 
+
+
+	 
+
 func _physics_process(delta):
 	var move_vec = Vector3()
+	var speed_Modifier = 1
+
 	if Input.is_action_pressed("move_forwards"):
 		move_vec.z -= 1
 	if Input.is_action_pressed("move_backwards"):
@@ -39,9 +52,31 @@ func _physics_process(delta):
 		move_vec.x -= 1
 	if Input.is_action_pressed("move_right"):
 		move_vec.x += 1
+	
+	move_vec.y  = 0
+	move_vec = move_vec.rotated(Vector3(0,1,0), rotate)
 	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_and_collide(move_vec * MOVE_SPEED * delta)
+	
+	velocity.y += delta * gravity
+	
+	var hv = velocity
+	hv.y = 0
+	
+	var new_pos = move_vec * MOVE_SPEED
+	var accel = DE_ACCELERATION
+	
+	if (move_vec.dot(hv) > 0):
+		accel = ACCELERATION
+	
+	hv = hv.linear_interpolate(new_pos, accel*delta)
+	
+	velocity.x = hv.x
+	velocity.z = hv.z
+	
+	
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+#	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+#	move_and_collide(move_vec * (MOVE_SPEED * speed_Modifier) * delta)
    
 	if Input.is_action_pressed("attack") and !anim_player.is_playing():
 		anim_player.play("attack")
